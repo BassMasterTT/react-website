75ade53ae2baa44b165b70466d6cf553320a4025
BassMasterTT <tothtibi22@gmail.com>
2021-02-15 19:05:56 +0800

    App.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <>
       <nav className="navbar">
-          <div className="navbar-container">
-                <Link to="/" className="navbar-logo">
-                    TRVL
-                </Link>
-          </div>
-
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            TRVL <i className="fab fa-typo3" />
+          </Link>
+        </div>
       </nav>
     </>
   );
