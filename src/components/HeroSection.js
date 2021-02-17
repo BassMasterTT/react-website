f14524e4fd6eb6f6326943d649dc15d941d903a9
BassMasterTT <tothtibi22@gmail.com>
2021-02-18 00:57:14 +0800

    final. everything working. hero picture instead of video

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-2.mp4" autoPlay loop muted />
+      {/* <video src="/videos/video-2.mp4" autoPlay loop muted /> */}
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you awaiting for?</p>
       <div className="hero-btns">
@@ -22,7 +22,7 @@ function HeroSection() {
           buttonStyle="btn--primary"
           buttonSize="btn--large"
         >
-          WATCH TRAILER <i className='far fa-play-circle' />
+          WATCH TRAILER <i className="far fa-play-circle" />
         </Button>
       </div>
     </div>
